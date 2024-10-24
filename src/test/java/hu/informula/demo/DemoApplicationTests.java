3f898159f3f05facca1c57166e2d171fe28dac01
csehof <ferenc.cseho@frontside.hu>
2024-10-24 09:42:24 +0200

    docker-compose and testcontainers

--- src/test/java/hu/informula/demo/DemoApplicationTests.java
+++ src/test/java/hu/informula/demo/DemoApplicationTests.java
@@ -1,13 +1,55 @@
 package hu.informula.demo;
 
+import hu.informula.demo.controller.MovieController;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Testcontainers
 class DemoApplicationTests {
+
+    @Autowired
+    private MovieController controller;
+
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:8.0")
+            .withDatabaseName("testdb")
+            .withUsername("test")
+            .withPassword("test");
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>("redis:latest")
+            .withExposedPorts(6379);
+
+    @AfterAll
+    static void afterAll() {
+        mysql.stop();
+        redis.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+        registry.add("spring.redis.host", redis::getHost);
+        registry.add("spring.redis.port", () -> redis.getMappedPort(6379));
+    }
 
     @Test
     void contextLoads() {
+        assertThat(controller).isNotNull();
     }
-
 }
+
+
